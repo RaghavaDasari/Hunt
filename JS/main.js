596fcc1d7b9ca3062b8edac1a9bb20f37e575eb7
Raghava Dasari <draghavalee007@gmail.com>
2022-09-08 04:27:10 +0530

    slip Style

---
+++ JS/main.js
@@ -0,0 +1,30 @@
+function check(str){
+    if(str == "slip1"){
+        var x = document.getElementsByClassName('input');
+        alert(x[0].value);
+    }else if(str == "slip2"){
+        var x = document.getElementsByClassName('input');
+        alert(x[0].value);
+    }else if(str == "slip3"){
+        var x = document.getElementsByClassName('input');
+        alert(x[0].value + "slip3");
+    }else if(str == "slip4"){
+        var x = document.getElementsByClassName('input');
+        alert(x[0].value);
+    }else if(str == "slip5"){
+        var x = document.getElementsByClassName('input');
+        alert(x[0].value);
+    }else if(str == "slip6"){
+        var x = document.getElementsByClassName('input');
+        alert(x[0].value);
+    }else if(str == "slip7"){
+        var x = document.getElementsByClassName('input');
+        alert(x[0].value);
+    }else if(str == "slip8"){
+        var x = document.getElementsByClassName('input');
+        alert(x[0].value);
+    }else if(str == "slip9"){
+        var x = document.getElementsByClassName('input');
+        alert(x[0].value);
+    }
+}
