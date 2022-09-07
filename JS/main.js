8d4962ceadf171226f92be1689f5c4d87942487f
Raghava Dasari <draghavalee007@gmail.com>
2022-09-08 04:45:14 +0530

    Slips Updated and Conditions added

--- JS/main.js
+++ JS/main.js
@@ -1,30 +1,75 @@
 function check(str){
     if(str == "slip1"){
         var x = document.getElementsByClassName('input');
-        alert(x[0].value);
+        if(x[0].value == "huggies"){
+            alert("I know you will do it my love...!!!");
+            alert("For your next Clue, Just go to *thgiL nooM*");
+        }else{
+            alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
+        }
     }else if(str == "slip2"){
         var x = document.getElementsByClassName('input');
-        alert(x[0].value);
+        if(x[0].value == "star"){
+            alert("I know you will do it my love...!!!");
+            alert("For your next Clue, Just go to *thgiL nooM*");
+        }else{
+            alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
+        }
     }else if(str == "slip3"){
         var x = document.getElementsByClassName('input');
-        alert(x[0].value + "slip3");
+        if(x[0].value == "keychain"){
+            alert("I know you will do it my love...!!!");
+            alert("For your next Clue, Just go to *thgiL nooM*");
+        }else{
+            alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
+        }
     }else if(str == "slip4"){
         var x = document.getElementsByClassName('input');
-        alert(x[0].value);
+        if(x[0].value == "elephant"){
+            alert("I know you will do it my love...!!!");
+            alert("For your next Clue, Just go to *thgiL nooM*");
+        }else{
+            alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
+        }
     }else if(str == "slip5"){
         var x = document.getElementsByClassName('input');
-        alert(x[0].value);
+        if(x[0].value == "food"){
+            alert("I know you will do it my love...!!!");
+            alert("For your next Clue, Just go to *thgiL nooM*");
+        }else{
+            alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
+        }
     }else if(str == "slip6"){
         var x = document.getElementsByClassName('input');
-        alert(x[0].value);
+        if(x[0].value == "handle"){
+            alert("I know you will do it my love...!!!");
+            alert("For your next Clue, Just go to *thgiL nooM*");
+        }else{
+            alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
+        }
     }else if(str == "slip7"){
         var x = document.getElementsByClassName('input');
-        alert(x[0].value);
+        if(x[0].value == "maintenance"){
+            alert("I know you will do it my love...!!!");
+            alert("For your next Clue, Just go to *thgiL nooM*");
+        }else{
+            alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
+        }
     }else if(str == "slip8"){
         var x = document.getElementsByClassName('input');
-        alert(x[0].value);
+        if(x[0].value == "fujifilm"){
+            alert("I know you will do it my love...!!!");
+            alert("For your next Clue, Just go to *thgiL nooM*");
+        }else{
+            alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
+        }
     }else if(str == "slip9"){
         var x = document.getElementsByClassName('input');
-        alert(x[0].value);
+        if(x[0].value == "golden"){
+            alert("I know you will do it my love...!!!");
+            alert("For your next Clue, Just go to *thgiL nooM*");
+        }else{
+            alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
+        }
     }
 }
