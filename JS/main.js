176b784876616814a424cc634f7e6141529896ce
Raghava Dasari <draghavalee007@gmail.com>
2022-09-08 05:58:22 +0530

    Final Done

--- JS/main.js
+++ JS/main.js
@@ -3,71 +3,74 @@ function check(str){
         var x = document.getElementsByClassName('input');
         if(x[0].value == "huggies"){
             alert("I know you will do it my love...!!!");
-            alert("For your next Clue, Just go to *thgiL nooM*");
+            alert("For your next Clue, Just go to Place where you *Fly your wings under the light...!!!*");
         }else{
             alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
         }
     }else if(str == "slip2"){
         var x = document.getElementsByClassName('input');
         if(x[0].value == "star"){
-            alert("I know you will do it my love...!!!");
-            alert("For your next Clue, Just go to *thgiL nooM*");
+            alert("You are progressing...!!!");
+            alert("For your next Clue, Just go to *Missing Lette_ 🥇🤫eks*");
         }else{
             alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
         }
     }else if(str == "slip3"){
         var x = document.getElementsByClassName('input');
         if(x[0].value == "keychain"){
-            alert("I know you will do it my love...!!!");
-            alert("For your next Clue, Just go to *thgiL nooM*");
+            alert("Appreciating your efforts little one...!!!");
+            alert("Riddle Time...!! Hehehehe");
+            alert("Though I’m not an elephant, I am something with a trunk, if I get old and rusty, you might sell me off as junk. What am I?");
+            alert("Find me, Reach me, get your next clue...!!!");
         }else{
             alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
         }
     }else if(str == "slip4"){
         var x = document.getElementsByClassName('input');
         if(x[0].value == "elephant"){
-            alert("I know you will do it my love...!!!");
-            alert("For your next Clue, Just go to *thgiL nooM*");
+            alert("Pattu vadhalani vikramaarka, Ni bethaaludini neney...!!!");
+            alert("Cake Roven lo Bill eseyvaadiki dabbulu lekka pettatam ochu kaani, enugu spelling raadhu anta nerpinchu velli...!!!");
         }else{
             alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
         }
     }else if(str == "slip5"){
         var x = document.getElementsByClassName('input');
         if(x[0].value == "food"){
-            alert("I know you will do it my love...!!!");
-            alert("For your next Clue, Just go to *thgiL nooM*");
+            alert("Naaku ardham ayipoyindhi jaanu, Nuvvem thinaledhu kadha...!!! Andhukey Food Aduguthunnav...!!!");
+            alert("Peaceful ge kurchoni order ivvu tarvatha continue chedhaam ee game...!!!");
         }else{
             alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
         }
     }else if(str == "slip6"){
         var x = document.getElementsByClassName('input');
         if(x[0].value == "handle"){
-            alert("I know you will do it my love...!!!");
-            alert("For your next Clue, Just go to *thgiL nooM*");
+            alert("Baagaa thinnava fooduuu...!!!");
+            alert("Inkem mari velli ra, niku kuda naalaagey stomach issue kadha, Thaggipoyina parledhu velli ra po...!!!");
         }else{
             alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
         }
     }else if(str == "slip7"){
         var x = document.getElementsByClassName('input');
         if(x[0].value == "maintenance"){
-            alert("I know you will do it my love...!!!");
-            alert("For your next Clue, Just go to *thgiL nooM*");
+            alert("Ikkada Cleaning chaalaa baagundhi kadha...!!!");
+            alert("Sarele Hadaavudi antha ayipoyindhi late endhuku inka cake cut chedhaam pattu...!!!");
         }else{
             alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
         }
     }else if(str == "slip8"){
         var x = document.getElementsByClassName('input');
         if(x[0].value == "fujifilm"){
-            alert("I know you will do it my love...!!!");
-            alert("For your next Clue, Just go to *thgiL nooM*");
+            alert("Intha dhooram ochaav, that too nannu nammi, ninnu odhilesi ekkadiki vellanu ra dimbu.");
+            alert("Inka ni deggara ki ostha, na deggara unna... niku kaavalsina... undhi na deggara thisuko");
         }else{
             alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
         }
     }else if(str == "slip9"){
         var x = document.getElementsByClassName('input');
         if(x[0].value == "golden"){
-            alert("I know you will do it my love...!!!");
-            alert("For your next Clue, Just go to *thgiL nooM*");
+            alert("After a lot of struggle, You reached me...!!!*");
+            alert("Ni Final gift neney ra dimbu, I Love you.");
+            alert("Ni Happiness avudhaam anukunnanu, Ni Happiness ni laagesukunnanu. Malli idhaam anukuntunna elano thelusaaa??");
         }else{
             alert("Try Again My lil one...!\nOnly thing you have to do is 'Think'\nMake Sure You Enter your code in smalls.");
         }
